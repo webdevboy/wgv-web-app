5ce4aae1892fc0bcacd591da9f7bef9423a83c17
webdevboy <piao@whitelabelapplications.co.uk>
2018-06-23 04:10:28 +0100

    First Commit

---
+++ src/containers/prescription.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+import { bindActionCreators, compose } from 'redux'
+import { push, replace } from 'react-router-redux'
+
+// Material UI
+import { withStyles } from '@material-ui/core/styles'
+import Slide from '@material-ui/core/Slide'
+
+// Components
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+      padding: 20,
+      width: '100%',
+      minHeight: 'calc(100% - 156px)',
+      [theme.breakpoints.down('md')]: {
+        marginTop: 20,
+      },
+    },
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      width: 800,
+      position: 'relative',
+    },
+    contentHeader: {
+      display: 'flex',
+      justifyContent: 'center',
+      width: '100%',
+      padding: '30px 0',
+      textAlign: 'center',
+      borderBottom: '3px solid #cfe1e3',
+    },
+    title: {
+      fontSize: 40,
+      fontWeight: 300,
+      color: '#757575',
+      [theme.breakpoints.down('md')]: {
+        fontSize: 30,
+      },
+    },
+    link: {
+      lineHeight: 2,
+      fontSize: 12,
+      color: '#8bb4c2',
+      marginTop: 40,
+    },
+    contents: {
+      display: 'flex',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+      minHeight: 'calc(100vh - 370px)',
+    },
+    grid: {
+      backgroundColor: '#cfe1e3',
+      margin: '100px 0',
+      minHeight: 350,
+      [theme.breakpoints.down('md')]: {
+        margin: '50px 0',
+      },
+    },
+})
+
+class Prescription extends Component {
+
+    render() {
+      
+      const { classes } = this.props
+      
+      return (
+          <div className={classes.root}>
+            <div className={classes.contentHeader}>
+                <div className={classes.container}>
+                    <div className={classes.title}>Your prescription</div>
+                    <div className={classes.link}>focused on details</div>
+                </div>
+            </div>
+            <div className={classes.contents}>
+                <div className={classes.container}>
+                <Slide direction="left" in={true} mountOnEnter unmountOnExit>
+                  <div className={classes.grid}>
+                    
+                  </div>
+                </Slide>
+                </div>
+            </div>
+          </div>
+      )
+	}
+}
+
+Prescription.propTypes = {
+	classes: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = () => ({})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+	push,
+	replace
+}, dispatch)
+
+export default compose(
+	withStyles(styles), connect(mapStateToProps, mapDispatchToProps)
+)(Prescription)
