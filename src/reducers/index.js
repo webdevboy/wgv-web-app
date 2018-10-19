f2c0c034a2b56f81a948dcbc93e97422c0829800
webdevboy <piao@whitelabelapplications.co.uk>
2018-10-19 03:02:21 +0100

    Updates

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import notification from './notification'
-import auth from './auth'
-
-export default {
-	notification,
-	auth,
-}
--- src/util/selectors.js
+++
@@ -1,5 +0,0 @@
-import {
-	map, assoc, propOr, prop, isEmpty,
-} from 'ramda'
-
-import { createSelector } from 'reselect'
